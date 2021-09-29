bd3e67af840791aacc782738b7761953f0a878e4
Chris P <chrisnatepollock@gmail.com>
2021-09-29 16:10:49 -0400

    F2P Wiki XP Tracker

--- src/test/java/com/f2pwiki/f2pwikiPluginTest.java
+++ src/test/java/com/f2pwiki/f2pwikiPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.f2pwiki;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class f2pwikiPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(f2pwikiPlugin.class);
 		RuneLite.main(args);
 	}
 }
